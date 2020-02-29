e3c6f7a3e8e3d07f82b201f091111a76bc889218
Slava <slava91petrushin@yandex.ru>
2020-02-29 10:51:52 +0300

    Updates

--- precache-manifest.b82ef6f47ea28d29bea0a45f17eb355c.js
+++ precache-manifest.b82ef6f47ea28d29bea0a45f17eb355c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ef4002208b0672a3c6f25f7dc043706",
+    "revision": "f32d20cd195be98a527f1a433f21a753",
     "url": "/todolist/index.html"
   },
   {
-    "revision": "1d986a86ae7d8cf6fa90",
-    "url": "/todolist/static/css/main.d6ccf511.chunk.css"
+    "revision": "1035d46bb890a0d9cd43",
+    "url": "/todolist/static/css/main.10d629c9.chunk.css"
   },
   {
     "revision": "9ffaa483e21e53cb65cf",
     "url": "/todolist/static/js/2.9f4aff80.chunk.js"
   },
   {
-    "revision": "1d986a86ae7d8cf6fa90",
-    "url": "/todolist/static/js/main.f4e2e45f.chunk.js"
+    "revision": "1035d46bb890a0d9cd43",
+    "url": "/todolist/static/js/main.d07c1082.chunk.js"
   },
   {
     "revision": "5225356577f332e6df46",
